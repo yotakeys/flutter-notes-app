e8a4faf2869ead2abef806d22b537e3710cf366d
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2024-04-17 08:36:04 +0700

    feat: add notes app basic crud

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_notes_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_notes_app/page/note_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  static String title = 'Notes SQLite';
+
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      home: const NotesPage(),
+    );
+  }
+}
